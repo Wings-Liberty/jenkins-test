d5b80894b6abdd2cb27d20465f9db322c13efc8c
Wings-Liberty <1434948003@qq.com>
2020-09-30 14:35:03 +0800

    '去掉了多余的接口，只剩下一个/index接口。此接口返回index.html页面'

--- src/main/java/com/cx/controller/TestController.java
+++ src/main/java/com/cx/controller/TestController.java
@@ -2,51 +2,15 @@ package com.cx.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @CrossOrigin
 public class TestController {
 
-//    @GetMapping("/hello")
-//    @ResponseBody
-//    public String test(){
-//        System.out.println("hello");
-//        return "hello";
-//    }
-//
-//    @GetMapping("/test")
-//    public String toTest(){
-//        System.out.println("test");
-//        return "test";
-//    }
-//
-//    @GetMapping("/str")
-//    @ResponseBody
-//    public String str(String str){
-//        System.out.println(str);
-//        str = "success";
-//        return str;
-//    }
-//
-//    @GetMapping("/obj")
-//    @ResponseBody
-//    public User user( User user){
-//        System.out.println(user);
-//        return new User(100, "java", "java", "java");
-//    }
-//
-//    @GetMapping("/get")
-//    @ResponseBody
-//    public String get(){
-//        return "get";
-//    }
-//
-//    @PostMapping("/post")
-//    @ResponseBody
-//    public User post(User user){
-//        System.out.println(user);
-//        return new User(100, "java", "java", "java");
-//
-//    }
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
 
 }
